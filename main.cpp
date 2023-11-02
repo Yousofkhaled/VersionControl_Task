b7685f0eed302ee7fdb9fde40a81faa24e6d9536
Yousof Khaled <yk849754@gmail.com>
2023-11-02 15:00:57 +0200

    added getMin

--- main.cpp
+++ main.cpp
@@ -12,12 +12,24 @@ int getSum (vector<int> &v) {
     return sum;
 }
 
+int getMin (vector<int> &v) {
+    
+    int mn = v[0];
+    for (auto num : v) {
+        mn = min (mn, num);
+    }
+    
+    return mn;
+}
+
 int main () {
 
 
     vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     cout << "sum = " << getSum (v) << "\n";
+    
+    cout << "min = " << getMin (v) << "\n";
 
     return 0;
 }
